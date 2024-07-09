60bf1d057477fe29d694f4596a699050d673fc97
Patrik Hermansson <hermansson.patrik@gmail.com>
2024-07-09 16:48:50 +0200

    Mock DB Tests works

--- MyLibrary.Test/MockTestSearch.cs
+++ MyLibrary.Test/MockTestSearch.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MyLibrary.Data;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Telerik.JustMock.AutoMock.Ninject.Activation;
 
 namespace MyLibrary.Test
 {
@@ -20,33 +15,63 @@ namespace MyLibrary.Test
 		{
 			context = new MyLibraryContext(dbContextOptions);
 			context.Database.EnsureCreated();
+			
+		}
+
+		[Fact]
+		public void CheckTitleOfBookTwo()
+		{
+			// Arrange
+			SeedDatabase();
+			Models.Book book;
+
+			// Act
+			//Do not use id's, they are unpredictable (changes between runs).
+			//book = context.Book.Find(3);
+			book = context.Book.Where(s => s.Author == "Stephen King").FirstOrDefault();
+
+			// Assert
+			Assert.Equal("Det", book.Name);
+		}
+		[Fact]
+		public void CheckAuthorOfBookThree()
+		{
+			SeedDatabase();
+			Models.Book book;
+			book = context.Book.Where(s => s.Name == "Trumslagarpojken").FirstOrDefault();
+
+			Assert.Equal("Anders Johansson", book.Author);
 		}
 		private void SeedDatabase()
 		{
+
 			List<Models.Book> mockbooks = new List<Models.Book>()
 			{
 				new Models.Book()
 				{
-					Id=1,
+					//Id=1,
 					Name="Röda rummet",
 					Author="August Strindberg"
 				},
 				new Models.Book()
 				{
-					Id=2,
+					//Id=2,
 					Name="Det",
 					Author="Stephen King"
 				},
 				new Models.Book()
 				{
-					Id=3,
+					//Id=3,
 					Name="Trumslagarpojken",
 					Author="Anders Johansson"
 				}
 
 			};
-			context.Book.AddRange(mockbooks);
-			context.SaveChanges();
+			//if (mockbooks.Count == 0)
+			//{
+				context.Book.AddRange(mockbooks);
+				context.SaveChanges();
+			//}
 		}
 		public void Dispose()
 		{
