4c9f255df4bd863283c9ffe51c88f742b2727354
Patrik Hermansson <hermansson.patrik@gmail.com>
2024-07-14 14:37:15 +0200

    Can list all books.

--- MyLibraryGUI/Pages/Privacy2.cshtml.cs
+++ MyLibraryGUI/Pages/Privacy2.cshtml.cs
@@ -12,16 +12,11 @@ namespace MyLibrary.GUI.Pages
         
         public void OnGet()
         {
-
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri("https://localhost:7034/");
             HttpResponseMessage message = client.GetAsync("api/Books").Result;
             string returnText = message.Content.ReadAsStringAsync().Result;
-            var bookList = JsonSerializer.Deserialize<List<Book>>(returnText);
-
-            var t = new Book { Author = "P", BookId = 1, Name = "Wow" };
-            Books.Add(t);
+            Books = JsonSerializer.Deserialize<List<Book>>(returnText);
         }
     }
-
 }
