072fa1b3ae70e2afdfe50db6eb32a4f15d25c416
Patrik Hermansson <hermansson.patrik@gmail.com>
2024-07-15 11:34:14 +0200

    Search working

--- MyLibraryGUI/Pages/Index.cshtml.cs
+++ MyLibraryGUI/Pages/Index.cshtml.cs
@@ -8,32 +8,51 @@ namespace MyLibraryGUI.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-        [Required]
-        [BindProperty]
-        public string Password { get; set; }
+        //private readonly ILogger<IndexModel> _logger;
+        //[Required]
+        //[BindProperty]
+        //public string SearchString { get; set; }
         public List<Book> bookList { get; set; }
+        //public IndexModel(ILogger<IndexModel> logger)
+        //{
+        //    _logger = logger;
+        //}
+        [TempData]
+        public string SearchString { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+        //public void OnGet()
+        //{
+        //if(SearchString != null)
+        //{
+        //    return RedirectToPage("/SearchResult");
+        //}
+        //return Redirect("~/");
+
+
+        //HttpClient client = new HttpClient();
+        //client.BaseAddress = new Uri("https://localhost:7034/");
+        //HttpResponseMessage message = client.GetAsync("api/Users").Result;
+        //string returnText = message.Content.ReadAsStringAsync().Result;
+
+        //message = client.GetAsync("api/Books").Result;
+        //returnText = message.Content.ReadAsStringAsync().Result;
+
+        //List<Book> bookList = new List<Book>();
+        //bookList =
+        //    JsonSerializer.Deserialize<List<Book>>(returnText);
+        //}
 
         public void OnGet()
         {
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("https://localhost:7034/");
-            HttpResponseMessage message = client.GetAsync("api/Users").Result;
-            string returnText = message.Content.ReadAsStringAsync().Result;
+  
+        }
 
-            message = client.GetAsync("api/Books").Result;
-            returnText = message.Content.ReadAsStringAsync().Result;
+        public IActionResult OnPost()
+        {
+            SearchString = Request.Form["SearchString"];
+            //SearchString = "Incorrect password";
 
-            List<Book> bookList = new List<Book>();
-            bookList =
-                JsonSerializer.Deserialize<List<Book>>(returnText);
-
-            Password = "Test";
+            return RedirectToPage("/SearchResult");
         }
     }
 }
--- MyLibraryGUI/Pages/SearchResult.cshtml.cs
+++ MyLibraryGUI/Pages/SearchResult.cshtml.cs
@@ -1,18 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using MyLibrary.Models;
 using System.Text.Json;
 
 namespace MyLibrary.GUI.Pages
 {
-    public class ListAllModel : PageModel
+    public class SearchModel : PageModel
     {
+        [BindProperty(SupportsGet = true)]
+        //public string? SearchString { get; set; }
         public IList<Book> Books { get; set; } = default!;
-        
+
         public void OnGet()
         {
+            var SearchString = TempData["SearchString"];
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri("https://localhost:7034/");
-            HttpResponseMessage message = client.GetAsync("api/Books").Result;
+            HttpResponseMessage message = client.GetAsync("api/Books/"+SearchString).Result;
             string returnText = message.Content.ReadAsStringAsync().Result;
             Books = JsonSerializer.Deserialize<List<Book>>(returnText);
         }
