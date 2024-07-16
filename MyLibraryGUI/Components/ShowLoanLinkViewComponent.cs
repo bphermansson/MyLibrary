d4f20c04ae74dda8e71d6d16cb707b31ffcbab2c
Patrik Hermansson <hermansson.patrik@gmail.com>
2024-07-16 19:15:59 +0200

    ViewComponent added.

---
+++ MyLibraryGUI/Components/ShowLoanLinkViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyLibrary.GUI.Models;
+
+namespace MyLibrary.Components
+{
+    public class ShowLoanLinkViewComponent : ViewComponent
+    {
+
+        // Get data to display. Can from a db, api, or as here just a static number.
+
+        public IViewComponentResult Invoke()
+        {
+            var linkClass = new LinkClass();
+            linkClass.Text = "Låna";
+            linkClass.Url = "/Lend";
+            //List<string> link = new List<string> { "Låna", "/Lend" };
+            return View("Default", linkClass);
+        }
+    }
+}
---
+++ MyLibraryGUI/Models/LinkClass.cs
@@ -0,0 +1,8 @@
+﻿namespace MyLibrary.GUI.Models
+{
+    public class LinkClass
+    {
+        public string Text { get; set; }
+        public string Url { get; set; }
+    }
+}
