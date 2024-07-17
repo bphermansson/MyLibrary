e78548ae7a3a59a8ee7122e076b07c05ed25c471
Patrik Hermansson <hermansson.patrik@gmail.com>
2024-07-17 14:53:13 +0200

    Gui can send a loan request to the api.

--- MyLibraryGUI/Pages/Lend.cshtml.cs
+++ MyLibraryGUI/Pages/Lend.cshtml.cs
@@ -5,10 +5,12 @@ namespace MyLibrary.GUI.Pages
 {
     public class LendModel : PageModel
     {
-        public void OnGet()
+        public void OnGet(int book, int user)
         {
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri("https://localhost:7034/");
+            Console.WriteLine(book);
+
             //HttpResponseMessage message = client.GetAsync("api/Books/" + SearchString).Result;
             //string returnText = message.Content.ReadAsStringAsync().Result;
         }
